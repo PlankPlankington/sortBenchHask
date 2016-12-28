22e28e90d88bbca90a8ef14ebd6d9ec73da06ce8
PlankPlankington <ttomgzombiez@gmail.com>
2016-12-27 18:41:11 -0800

    Added bubblesort

--- quickBench.hs
+++ quickBench.hs
@@ -15,14 +15,35 @@ quicksort list = case list of
                 top = quicksort [a | a <- xs, a > x]
             in bottom ++ [x] ++ top
 
+bubbleHelp :: [Int] -> [Int]
+bubbleHelp list = case list of
+    x:y:xs -> if y > x then x:bubbleHelp (y:xs) else y:bubbleHelp (x:xs)
+    [x] -> [x]
+    [] -> []
+
+bubble :: Int -> [Int] -> [Int]
+bubble l xs = case l of
+    0 -> xs
+    n -> bubble (n-1) (bubbleHelp xs)
+
+bubblesort :: [Int] -> [Int]
+bubblesort list = bubble (length list) list
+
 transfer :: [Int] -> [Int] -> [Int] -> [Int] -> [Int] -> IO ()
 transfer la lb lc ld le = defaultMain [
-    bgroup "quick" [ 
-        bench "100" $ whnf quicksort la,
-        bench "1000" $ whnf quicksort lb,
-        bench "10000" $ whnf quicksort lc,
-        bench "100000" $ whnf quicksort ld,
-        bench "1000000" $ whnf quicksort le
+    bgroup "Bubble Sort" [ 
+        bench "100 Elements" $ whnf bubblesort la,
+        bench "1000 Elements" $ whnf bubblesort lb,
+        bench "10000 Elements" $ whnf bubblesort lc,
+        bench "100000 Elements" $ whnf bubblesort ld,
+        bench "1000000 Elements" $ whnf bubblesort le
+        ],
+    bgroup "Quick Sort" [ 
+        bench "100 Elements" $ whnf quicksort la,
+        bench "1000 Elements" $ whnf quicksort lb,
+        bench "10000 Elements" $ whnf quicksort lc,
+        bench "100000 Elements" $ whnf quicksort ld,
+        bench "1000000 Elements" $ whnf quicksort le
         ]
     ]
 
